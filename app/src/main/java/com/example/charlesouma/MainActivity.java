9c708a0fb49e3f539088e3a9df769eaf9be264e7
AC03 <oderocharlesouma@gmail.com>
2019-03-07 18:19:35 +0300

    initial comit

--- app/src/main/java/com/example/charlesouma/MainActivity.java
+++ app/src/main/java/com/example/charlesouma/MainActivity.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
 public class MainActivity extends AppCompatActivity {
 
+    EditText  usernameText;
+
     EditText passwordText;
+
+    EditText confirmPasswordText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +28,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void myfirstbutton(View view){
 
+        EditText   usernameText = (EditText) findViewById(R.id.edittextusername);
         EditText  passwordText = (EditText) findViewById(R.id.edittextpassword);
+        EditText  confirmPasswordText = (EditText)  findViewById(R.id.edittextconfirmpassword);
 
-        Toast.makeText(MainActivity.this, passwordText.getText().toString(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, usernameText.getText().toString(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, passwordText.getText() .toString(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, confirmPasswordText.getText() .toString(), Toast.LENGTH_SHORT).show();
 
 
     }
